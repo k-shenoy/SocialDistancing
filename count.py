edc06105b862ad4a3c6068abc4b496c406b8bc2a
Keshav Shenoy <keshavsy@gmail.com>
2020-05-02 22:32:51 -0400

    Finetuning python script

--- count.py
+++ count.py
@@ -1,29 +1,70 @@
 import face_recognition
 import cv2
 import numpy as np
-import random
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import db
+import pandas as pd
 
+def postFunc(data, optNum):
+    num = 1
+    while num != 140:
+        if (optNum == 1):
+            ref.child(str(num)).update({
+                'long': data['location'][num - 1].split(",")[0],
+                'lat': data['location'][num - 1].split(",")[1],
+                'value': data['value'][num - 1]
+            })
+        else:
+            ref.child(str(num)).update({
+                'value': data['value'][0]
+            })
+        num+=1
+
+#Initialize Dict and camera data
 data = {'id': [], 'location': [], 'value':[]}
+cameradata = pd.read_csv("Cameras_Data.csv")
 
+#Initialize Video
 videoName = "samplevideo.mp4"
-
 video = cv2.VideoCapture(videoName)
-x = 1
+
+#Get Certification
+cred = credentials.Certificate('cert.json')
+
+# Initialize the app with a custom auth variable
+firebase_admin.initialize_app(cred, {
+    'databaseURL': 'https://distancebetter.firebaseio.com/',
+})
+
+ref = db.reference('location')
+
+# num = 1
+# while num != 140:
+#     ref.child(str(num)).set({
+#         'long': data['location'][0].split(",")[0],
+#         'lat': data['location'][0].split(",")[1],
+#         'value': data['value'][0]
+#     })
+#     num+=1
+
+#while True:
 y = 1
 while y != 99:
+    x = 1
     while x!=140:
-        video.set(cv2.CAP_PROP_POS_FRAMES, x*100-y)
+        video.set(cv2.CAP_PROP_POS_FRAMES, x * 100 - y)
         ret, frame = video.read()
         rgb_frame = frame[:, :, ::-1]
         face_locations = face_recognition.face_locations(rgb_frame)
-        if (y == 1):
+        if (len(data['id']) < 140):
             data['id'].append(x)
-            templat = random.randrange(4365, 4392)/100
-            templong = random.randrange(-8000, -7925)/100
+            templat = cameradata['LATITUDE'][x]
+            templong = cameradata['LONGITUDE'][x]
             data['location'].append(str(templat) + "," + str(templong))
             data['value'].append(len(face_locations))
         else:
             data['value'][x] = len(face_locations)
         x+=1
-    print(data)
+    postFunc(data, y)
     y+=1
